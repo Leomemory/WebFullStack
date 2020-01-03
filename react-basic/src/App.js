054cd3ccf79f1b868068aac14e87f63be751493b
李江 <lijiang.0408@cashbus.com>
2020-01-03 15:39:54 +0800

    react-basic初始化

---
+++ react-basic/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AppTest from './components/AntdTest'
+
+function App() {
+  return (
+    <div className="App">
+       <AppTest></AppTest>
+    </div>
+  );
+}
+
+export default App;
